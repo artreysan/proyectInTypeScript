b9dbbb0141b20cd201e8a23a05682e19b444bdf5
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-08 19:47:34 -0500

    Minute 20:36

--- src/salida.js
+++ src/salida.js
@@ -1,1 +1,7 @@
 console.log("hello world");
+//types
+var myString = "Hello world";
+myString = 22 + "";
+myString.split(" ");
+var myNumber = 22;
+var myBool = true || false;
--- src/salida.ts
+++ src/salida.ts
@@ -1,1 +1,9 @@
 console.log("hello world");
+
+//types
+var myString:string = "Hello world";
+myString = 22+"";
+myString.split(" ");
+
+var myNumber:number = 22;
+var myBool:boolean = true || false;
